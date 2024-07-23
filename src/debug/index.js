83ea807990194539033f8e8c8f06ff5773bcfa8a
yannych1995 <yannymaly@gmal.com>
2024-07-23 10:14:36 -0500

    hoisting + debug

---
+++ src/debug/index.js
@@ -0,0 +1,8 @@
+var a = 'hello'
+
+function hello(){
+    let b= 'Hello Word';
+    const c = 'Hello Word!';
+    debugger;
+}
+hello()
---
+++ src/hoisting/index.js
@@ -0,0 +1,16 @@
+/*
+.var nameOfDog; //undefined
+
+console.log(nameOfDog);// SE llama  sin estar definido
+var nameOfDog = 'Elmo';
+console.log(nameOfDog);
+*/
+
+
+
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perro es ${elmo}`);
+}
+var elmo = 'Elmito' // var = 'elmo'; se elevo con undefined
